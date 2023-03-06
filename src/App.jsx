02c7f409bc736bcee58f5b1cedb61470c75ce055
Roman <sayga76239@gmail.com>
2023-03-06 10:43:21 +0100

    start_day_first

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<>Hello World</>}></Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/components/Pages/Movis.jsx
@@ -0,0 +1,5 @@
+const Movis = () => {
+
+};
+
+export default Movis;
