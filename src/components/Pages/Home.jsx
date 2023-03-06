2926c631e0b1bd18d75ed122688373b047cfe526
Roman <sayga76239@gmail.com>
2023-03-06 19:06:17 +0100

    фффф

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,9 +1,20 @@
 import { HeaderNav } from 'components/Header/HeaderNav/HeaderNav.jsx';
+import { useEffect, useState } from 'react';
+import { getFetchTrending } from 'components/Api/Api';
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getFetchTrending().then(({ data }) => {
+      setMovies(data.);
+    });
+  }, []);
+
   return (
     <>
       <HeaderNav />
+      {movies.map(movie => <div key={movie.id}>{movies.title}</div>)}
     </>
   );
 };
